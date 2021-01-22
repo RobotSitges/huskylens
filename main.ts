c1fe6f1b34b08f9013fe607b44be2700fd54b4db
RobotSitges <44520839+RobotSitges@users.noreply.github.com>
2021-01-22 16:54:14 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,20 @@
+let Info = ""
+basic.showIcon(IconNames.Heart)
+let Gent = 0
+radio.setGroup(21)
 DFRobotMaqueenPlus.I2CInit()
 huskylens.initI2c()
+huskylens.forgetLearn()
+huskylens.clearOSD()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_OBJECT_RECOGNITION)
+basic.showIcon(IconNames.StickFigure)
 basic.forever(function () {
-	
+    Info = DFRobotMaqueenPlus.readVersion()
+    Gent = huskylens.getBox(HUSKYLENSResultType_t.HUSKYLENSResultBlock)
+    radio.sendString(Info)
+    radio.sendNumber(Gent)
+})
+basic.forever(function () {
+    huskylens.request()
+    basic.showNumber(Gent)
 })
